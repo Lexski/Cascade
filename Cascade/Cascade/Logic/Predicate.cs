550a64a9e2d946255828186fe9aa5c46d965f8af
Alexander Kubiesa <akubiesa@btinternet.com>
2017-03-18 14:14:38 +0000

    Created new Predicate abstract class for creating predicates.

---
+++ Cascade/Cascade/Logic/Predicate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cascade.Logic
+{
+    abstract class Predicate
+    {
+        public static Predicate CreateAndRegister(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Predicate CreateAndRegister(string name, string alias)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Predicate CreateAndRegister(string name, Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Predicate CreateAndRegister(string name, string alias, Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
