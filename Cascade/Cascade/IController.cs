8c2905e815c32be354fce7ac776b7f0bc6ab59ae
Alexander Kubiesa <akubiesa@btinternet.com>
2017-03-18 02:27:35 +0000

    Added empty classes Formula and Expression in order to add placeholder methods to Controller.

--- Cascade/Cascade/IController.cs
+++ Cascade/Cascade/IController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cascade.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,22 @@ namespace Cascade
     // Represents something that implements Cascade commands.
     interface IController
     {
+        void DeclarePredicate(string name);
+
+        void DeclarePredicate(string name, string alias);
+
+        void DeclarePredicate(string name, Expression expression);
+
+        void DeclarePredicate(string name, string alias, Expression expression);
+
+        void DeclareTest(string name, Expression expression);
+
+        void DeclareTest(string name, string alias, Expression expression);
+
+        void Assert(Formula formula);
+
+        void Generate();
+
+        void Save(string filePath);
     }
 }
--- Cascade/Cascade/Logic/Controller.cs
+++ Cascade/Cascade/Logic/Controller.cs
@@ -8,5 +8,49 @@ namespace Cascade.Logic
 {
     class Controller : IController
     {
+        public void Assert(Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclarePredicate(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclarePredicate(string name, Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclarePredicate(string name, string alias)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclarePredicate(string name, string alias, Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclareTest(string name, Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeclareTest(string name, string alias, Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Generate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string filePath)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
